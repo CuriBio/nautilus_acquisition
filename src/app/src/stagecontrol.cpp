981a41bbce7c6363a53496f40cf9e2c8255850c8
Jason Person <jrperson@gmail.com>
2022-12-23 18:14:51 -0800

    stage control refactor, bump version

--- src/app/src/stagecontrol.cpp
+++ src/app/src/stagecontrol.cpp
@@ -9,6 +9,7 @@
 StageControl::StageControl(QWidget *parent) : QDialog(parent), ui(new Ui::StageControl) {
     ui->setupUi(this);
     m_tango = new TangoStage();
+    m_tango->GetCurrentPos(m_curX, m_curY);
 }
 
 StageControl::~StageControl() {
@@ -17,38 +18,37 @@ StageControl::~StageControl() {
 }
 
 void StageControl::SetRelativePosition(double x, double y) {
-    m_curX += x;
-    m_curY += y;
     m_tango->SetRelativePos(x, y, true);
+    m_tango->GetCurrentPos(m_curX, m_curY);
 }
 
 void StageControl::SetAbsolutePosition(double x, double y) {
-    m_curX = x; m_curY = y;
     m_tango->SetAbsolutePos(x, y, true);
+    m_tango->GetCurrentPos(m_curX, m_curY);
 }
 
 void StageControl::SetRelativeX(double x) {
-    m_curX += x;
     m_tango->SetRelativePos(x, 0, true);
+    m_tango->GetCurrentPos(m_curX, m_curY);
 }
 
 void StageControl::SetAbsoluteX(double x) {
-    m_curX = x;
-    m_tango->SetAbsolutePos(m_curX, m_curY, true);
+    m_tango->SetAbsolutePos(x, m_curY, true);
+    m_tango->GetCurrentPos(m_curX, m_curY);
 }
 
 void StageControl::SetRelativeY(double y) {
-    m_curY += y;
-    m_tango->SetRelativePos(0, m_curY, true);
+    m_tango->SetRelativePos(0, y, true);
+    m_tango->GetCurrentPos(m_curX, m_curY);
 }
 
 void StageControl::SetAbsoluteY(double y) {
-    m_curY = y;
-    m_tango->SetAbsolutePos(m_curX, m_curY, true);
+    m_tango->SetAbsolutePos(m_curX, y, true);
+    m_tango->GetCurrentPos(m_curX, m_curY);
 }
 
-const std::vector<LocationData*>& StageControl::GetLocations() const {
-    return m_locations;
+const std::vector<StagePosition*>& StageControl::GetPositions() const {
+    return m_positions;
 }
 
 //slots
@@ -58,12 +58,12 @@ void StageControl::on_deleteBtn_clicked() {
     if (row >= 0) {
         spdlog::info("Deleting item {}", row);
 
-        m_locations.erase(m_locations.begin()+row);
+        m_positions.erase(m_positions.begin()+row);
         auto i = ui->stageLocations->takeItem(row);
         delete i;
 
         size_t n = 1;
-        for (auto& i : m_locations) {
+        for (auto& i : m_positions) {
             i->pos = n++;
             i->setText(fmt::format("pos_{} - x: {}, y: {}", i->pos, i->x, i->y).c_str());
         }
@@ -71,10 +71,16 @@ void StageControl::on_deleteBtn_clicked() {
 }
 
 void StageControl::on_addBtn_clicked() {
-    int row = m_locations.size() + 1;
+    int row = m_positions.size() + 1;
+    double x, y;
+    m_tango->GetCurrentPos(x, y);
 
-    LocationData* item = new LocationData(row, row+m_curX, row+m_curY);
-    m_locations.push_back(item);
+    spdlog::info("Add current position x: {}, y: {}", x, y);
+    m_curX = x;
+    m_curY = y;
+
+    StagePosition* item = new StagePosition(row, m_curX, m_curY);
+    m_positions.push_back(item);
     ui->stageLocations->addItem(item);
 }
 
@@ -83,7 +89,7 @@ void StageControl::on_saveListBtn_clicked() {
 
     if (!file.isEmpty()) {
         toml::array vs{};
-        for (auto& v : m_locations) {
+        for (auto& v : m_positions) {
             vs.push_back(toml::value{ {"x", v->x}, {"y", v->y} });
         }
         const toml::value stage{ {"stage", toml::value{{"location", vs}}} };
@@ -101,8 +107,8 @@ void StageControl::on_loadListBtn_clicked() {
     if (!file.isEmpty()) {
         spdlog::info("Selected file: {}", file.toStdString());
 
-        for (auto v : m_locations) { delete v; }
-        m_locations.clear();
+        for (auto v : m_positions) { delete v; }
+        m_positions.clear();
         ui->stageLocations->clear();
 
         size_t n = 1;
@@ -113,8 +119,8 @@ void StageControl::on_loadListBtn_clicked() {
             auto x = static_cast<double>(v.at("x").as_floating());
             auto y = static_cast<double>(v.at("y").as_floating());
 
-            LocationData* item = new LocationData(pos, x, y);
-            m_locations.push_back(item);
+            StagePosition* item = new StagePosition(pos, x, y);
+            m_positions.push_back(item);
             ui->stageLocations->addItem(item);
         }
     }
@@ -125,9 +131,10 @@ void StageControl::on_gotoPosBtn_clicked() {
     int row = ui->stageLocations->currentRow();
 
     if (row >= 0) {
-        LocationData* item = static_cast<LocationData*>(ui->stageLocations->item(row));
+        StagePosition* item = static_cast<StagePosition*>(ui->stageLocations->item(row));
         spdlog::info("Setting stage to position x: {}, y: {}", item->x, item->y);
-        m_curX = item->x; m_curY = item->y;
+        m_tango->SetAbsolutePos(item->x, item->y, true);
+        m_tango->GetCurrentPos(m_curX, m_curY);
     } else {
         spdlog::info("Invalid selection");
     }
