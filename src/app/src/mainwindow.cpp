dced47d2b4f09db1c26900b519389ae1041d60fd
Luci Pak <luciypak@gmail.com>
2023-09-01 10:32:18 +1000

    update base plate svg

--- src/app/src/mainwindow.cpp
+++ src/app/src/mainwindow.cpp
@@ -828,8 +828,7 @@ void MainWindow::on_plateFormatDropDown_activated(int index) {
         }
     } else {
         for (size_t i = 0; i < PLATEMAP_COUNT; i++) {
-            // m_plateFormatImgs[i] = new QPixmap(QString("./resources/Nautilus-software_plate-base.png"));
-            m_plateFormatImgs[i] = new QPixmap(QString("./resources/Nautilus-software_24-well-plate-section1-active.svg"));
+            m_plateFormatImgs[i] = new QPixmap(QString("./resources/Nautilus-software_plate-base.svg"));
         }
     }
 
