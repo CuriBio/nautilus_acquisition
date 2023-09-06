196cef366989d7c7de1e1473dbd803d61937bb87
Luci Pak <luciypak@gmail.com>
2023-09-06 10:42:09 +1000

    update start live view

--- src/app/src/mainwindow.cpp
+++ src/app/src/mainwindow.cpp
@@ -463,7 +463,7 @@ void MainWindow::updateInputs() {
     ui.settingsBtn->setEnabled(testMask(SettingsMask));
     ui.stageNavigationBtn->setEnabled(testMask(StageNavigationMask) && !m_stageControl->isVisible());
 
-    if (!testMask(StageNavigationMask) && !m_stageControl->isVisible()) {
+    if (!testMask(StageNavigationMask)) {
         emit m_stageControl->sig_stage_disable_all();
     } else {
         emit m_stageControl->sig_stage_enable_all();
@@ -476,7 +476,7 @@ bool MainWindow::startLiveView() {
     spdlog::info("Starting liveview");
     //emit sig_disable_all();
 
-    setMask(LiveScanMask | StartAcquisitionMask | LedIntensityMask | (m_stageControl->isVisible() ? 0 : StageNavigationMask));
+    setMask(LiveScanMask | StartAcquisitionMask | LedIntensityMask | StageNavigationMask);
     emit m_stageControl->sig_stage_enable_all();
 
     double voltage = (m_config->ledIntensity / 100.0) * m_config->maxVoltage;
