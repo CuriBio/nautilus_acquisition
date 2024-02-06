9211f68928de9c2ba4dbdfbd2440c7aca0ffeaa5
luciipak <luciypak@gmail.com>
2024-02-06 13:26:49 +1100

    test

---
+++ libs/sdk_build/crt/aws-crt-cpp/generated/include/aws/crt/Config.h
@@ -0,0 +1,11 @@
+#pragma once
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#define AWS_CRT_CPP_VERSION "0.24.11"
+#define AWS_CRT_CPP_VERSION_MAJOR 0
+#define AWS_CRT_CPP_VERSION_MINOR 24
+#define AWS_CRT_CPP_VERSION_PATCH 11
+#define AWS_CRT_CPP_GIT_HASH "dd818f608b5b3b219d525554046a1776117e3996"
