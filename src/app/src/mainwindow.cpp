9eb453c1e2236e3b7ce3145a1d46c388a196d03c
luciipak <luciypak@gmail.com>
2024-02-09 08:22:14 +1100

    attempt download

--- src/app/src/mainwindow.cpp
+++ src/app/src/mainwindow.cpp
@@ -52,9 +52,9 @@
 #include <QPushButton>
 #include <aws/core/Aws.h>
 #include <aws/s3/S3Client.h>
+#include <aws/s3/S3EndpointProvider.h>
 #include <aws/s3/model/GetObjectRequest.h>
 #include <aws/core/auth/AWSCredentials.h>
-#include <aws/core/endpoint/AWSPartitions.h>
 
 #include "mainwindow.h"
 #include <PostProcess.h>
