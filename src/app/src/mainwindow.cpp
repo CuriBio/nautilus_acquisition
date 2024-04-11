d08c56cb6501e880584e8d6adcf22c6c38b016df
Jason Person <jason.person@curibio.com>
2024-04-11 08:00:55 -0700

    update

--- src/app/src/mainwindow.cpp
+++ src/app/src/mainwindow.cpp
@@ -1425,7 +1425,7 @@ void MainWindow::acquisitionThread(MainWindow* cls) {
 
         if (ax1->max() == frameCount) {
             l1[0]->setRange(ax1->min() + 5, ax1->max() + 5);
-            l2[0]->setRange(ax2->min() + 5, ax2->max() + 5);
+            //l2[0]->setRange(ax2->min() + 5, ax2->max() + 5);
             //l[0]->setRange(ax->max() - 490.0, ax->max() + 500.0 - 490.0);
             //frameCount = 0.0;
             //l[0]->setRange(0, 500.0);
