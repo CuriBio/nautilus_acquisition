8ecd307ec0b4132568f2d9a738e1eb799944e38c
Tanner Peterson <tannermp23@hotmail.com>
2024-10-07 15:05:09 -0500

    remove comments

--- src/app/src/plateidedit.cpp
+++ src/app/src/plateidedit.cpp
@@ -9,7 +9,6 @@ PlateIdEdit::PlateIdEdit(QWidget *parent) : QLineEdit(parent) { }
 void PlateIdEdit::focusInEvent(QFocusEvent *e) {
     QLineEdit::focusInEvent(e);
     // force completer to show when focus is gained, by default it won't show until a key is pressed
-    // TODO should wrap this in an if statement? and/or maybe need to change completion type based on the text
     this->completer()->complete();
 }
 
@@ -17,6 +16,5 @@ void PlateIdEdit::keyPressEvent(QKeyEvent *e) {
     QLineEdit::keyPressEvent(e);
     // force completer to show whenever a key is pressed. The completer will not show by default
     // if the text box is empty, this forces it to show anyway
-    // TODO should wrap this in an if statement? and/or maybe need to change completion type based on the text
     this->completer()->complete();
 }
